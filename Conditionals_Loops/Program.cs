034818d533576364567cd117177b74021098cd9c
anish102 <anishkarki695@gmail.com>
2024-06-25 22:14:27 +0545

    Use of conditionals and loops

---
+++ Conditionals_Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lst = new int[] { 1, 2, 5, 7, 8, 9, 43, 76 };
+            Console.WriteLine("Even numbers are:");
+            foreach (int i in lst)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
