d676d3feb35c5e9dc7b516a8376fbb8e9c7c3021
anish102 <anishkarki695@gmail.com>
2024-06-25 22:28:24 +0545

    OOP in C#

---
+++ OOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP
+{
+    class Animal
+    {
+
+        public string name;
+        public Animal()
+        {
+            name = "Dog";
+        }
+
+        public virtual void speak()
+        {
+            Console.WriteLine("Barks........");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("Meow.......");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+            myCat.speak();
+        }
+    }
+}
