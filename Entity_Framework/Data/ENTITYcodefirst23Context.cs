04fb78648fbc3f345bbbe88678ce05db0c2d53f1
anish102 <anishkarki695@gmail.com>
2024-07-06 20:53:25 +0545

    Entity framework

---
+++ Entity_Framework/Data/ENTITYcodefirst23Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ENTITYcodefirst23.Models;
+
+namespace ENTITYcodefirst23.Data
+{
+    public class ENTITYcodefirst23Context : DbContext
+    {
+        public ENTITYcodefirst23Context (DbContextOptions<ENTITYcodefirst23Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ENTITYcodefirst23.Models.Student> Student { get; set; } = default!;
+    }
+}
--- MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d676d3feb35c5e9dc7b516a8376fbb8e9c7c3021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8d708b027cf99873434e58d6f539f9d7447a28")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
